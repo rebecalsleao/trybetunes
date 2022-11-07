4a032fd8d3838bb3d4bc1ed6dc123adacc0178d9
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-11-07 17:03:07 -0300

    Ajuste

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Loading extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="loading">Carregando...</p>
+      </div>
+
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Loading from '../index.css';
 
 class Search extends React.Component {
   render() {
     return (
-      <div data-testid="page-search" />
+      <div data-testid="page-search">
+        <Loading className="loading">Carregando...</Loading>
+      </div>
+
     );
   }
 }
