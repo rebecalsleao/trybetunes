fae9c4ed988d7d7ed0de01aa4764f19537bac046
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-11-08 15:05:03 -0300

    REQUISITO 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+
+class Header extends React.Component {
+  state = {
+    name: {},
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const objectUser = await getUser();
+    this.setState({ name: objectUser.name, loading: false });
+  }
+
+  render() {
+    const { name, loading } = this.state;
+
+    if (loading) return (<Loading />);
+
+    return (
+
+      <header data-testid="header-component">
+        <p data-testid="header-user-name">{ name }</p>
+      </header>
+
+    );
+  }
+}
+
+export default Header;
