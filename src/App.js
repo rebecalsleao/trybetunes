59d566d9626ace812ff5a5b43caaf5661d8a647d
Rebeca Lins <rebeca.lsleao@gmail.com>
2022-11-09 09:53:53 -0300

    Requsito 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Search from './pages/Search';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,61 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-search">
-        <Header />
-      </div>
+  state = {
+    name: '',
+    isSaveButtonDisabled: true,
+  };
 
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.saveButtonDisabled();
+    });
+  };
+
+  saveButtonDisabled = () => {
+    const valueDisable = 2;
+    const { name } = this.state;
+    if (name.length < valueDisable) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    } else {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
+  };
+
+  render() {
+    const { name, isSaveButtonDisabled } = this.state;
+    return (
+      <section>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <input
+          data-testid="search-artist-input"
+          name="name"
+          value={ name }
+          type="text"
+          placeholder="Nome dos Artista"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          name="button"
+          onClick={ this.onSaveButtonClick }
+          disabled={ isSaveButtonDisabled }
+        >
+          Pesquisar
+
+        </button>
+      </section>
     );
   }
 }
